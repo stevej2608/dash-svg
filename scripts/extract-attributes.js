45ce7b86ced6962b6f44fdf267e5efde69cb224c
Steve Jones <jonesst2608@gmail.com>
2022-01-10 14:42:06 +0000

    Getting extract-attributes.js to work on SVG website

--- scripts/extract-attributes.js
+++ scripts/extract-attributes.js
@@ -1,106 +1,145 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path')
 const cheerio = require('cheerio');
-const request = require('request');
+const request = require('request-promise');
 const str = require('string');
+const flatCache = require('flat-cache');
 
-const htmlURL = 'https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes';
+const svgURL = 'https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute';
 const dataPath = './data/attributes.json';
 const htmlPath = './data/attributes.html';
 
-// From https://facebook.github.io/react/docs/tags-and-attributes.html#supported-attributes
-// less the special `className` and `htmlFor` props,
-// and `httpEquiv` + `acceptCharset` which are already correctly camelCased.
-const supportedAttributes = ['accept', 'accessKey', 'action', 'allow',
-'allowFullScreen', 'allowTransparency', 'alt', 'async', 'autoComplete',
-'autoFocus', 'autoPlay', 'capture', 'cellPadding', 'cellSpacing', 'challenge',
-'charSet', 'checked', 'cite', 'classID', 'colSpan', 'cols', 'content',
-'contentEditable', 'contextMenu', 'controls', 'coords', 'crossOrigin', 'data',
-'dateTime', 'default', 'defer', 'dir', 'disabled', 'download', 'draggable',
-'encType', 'form', 'formAction', 'formEncType', 'formMethod', 'formNoValidate',
-'formTarget', 'frameBorder', 'headers', 'height', 'hidden', 'high', 'href',
-'hrefLang', 'icon', 'id', 'inputMode', 'integrity', 'is',
-'keyParams', 'keyType', 'kind', 'label', 'lang', 'list', 'loop', 'low',
-'manifest', 'marginHeight', 'marginWidth', 'max', 'maxLength', 'media',
-'mediaGroup', 'method', 'min', 'minLength', 'multiple', 'muted', 'name',
-'noValidate', 'nonce', 'open', 'optimum', 'pattern', 'placeholder', 'poster',
-'preload', 'profile', 'radioGroup', 'readOnly', 'referrerPolicy', 'rel', 'required',
-'reversed', 'role', 'rowSpan', 'rows', 'sandbox', 'scope', 'scoped', 'scrolling',
-'seamless', 'selected', 'shape', 'size', 'sizes', 'span', 'spellCheck', 'src',
-'srcDoc', 'srcLang', 'srcSet', 'start', 'step', 'style', 'summary', 'tabIndex',
-'target', 'title', 'type', 'useMap', 'value', 'width', 'wmode', 'wrap'];
+// From https://reactjs.org/docs/dom-elements.html#differences-in-attributes
+//
+const supportedAttributes = ['accentHeight', 'accumulate', 'additive', 'alignmentBaseline', 'allowReorder', 'alphabetic',
+  'amplitude', 'arabicForm', 'ascent', 'attributeName', 'attributeType', 'autoReverse', 'azimuth',
+  'baseFrequency', 'baseProfile', 'baselineShift', 'bbox', 'begin', 'bias', 'by', 'calcMode', 'capHeight',
+  'clip', 'clipPath', 'clipPathUnits', 'clipRule', 'colorInterpolation',
+  'colorInterpolationFilters', 'colorProfile', 'colorRendering', 'contentScriptType',
+  'contentStyleType', 'cursor', 'cx', 'cy', 'd', 'decelerate', 'descent', 'diffuseConstant', 'direction',
+  'display', 'divisor', 'dominantBaseline', 'dur', 'dx', 'dy', 'edgeMode', 'elevation', 'enableBackground',
+  'end', 'exponent', 'externalResourcesRequired', 'fill', 'fillOpacity', 'fillRule', 'filter',
+  'filterRes', 'filterUnits', 'floodColor', 'floodOpacity', 'focusable', 'fontFamily', 'fontSize',
+  'fontSizeAdjust', 'fontStretch', 'fontStyle', 'fontVariant', 'fontWeight', 'format', 'from', 'fx', 'fy',
+  'g1', 'g2', 'glyphName', 'glyphOrientationHorizontal', 'glyphOrientationVertical', 'glyphRef',
+  'gradientTransform', 'gradientUnits', 'hanging', 'horizAdvX', 'horizOriginX', 'ideographic',
+  'imageRendering', 'in', 'in2', 'intercept', 'k', 'k1', 'k2', 'k3', 'k4', 'kernelMatrix', 'kernelUnitLength',
+  'kerning', 'keyPoints', 'keySplines', 'keyTimes', 'lengthAdjust', 'letterSpacing', 'lightingColor',
+  'limitingConeAngle', 'local', 'markerEnd', 'markerHeight', 'markerMid', 'markerStart',
+  'markerUnits', 'markerWidth', 'mask', 'maskContentUnits', 'maskUnits', 'mathematical', 'mode',
+  'numOctaves', 'offset', 'opacity', 'operator', 'order', 'orient', 'orientation', 'origin', 'overflow',
+  'overlinePosition', 'overlineThickness', 'paintOrder', 'panose1', 'pathLength',
+  'patternContentUnits', 'patternTransform', 'patternUnits', 'pointerEvents', 'points',
+  'pointsAtX', 'pointsAtY', 'pointsAtZ', 'preserveAlpha', 'preserveAspectRatio', 'primitiveUnits',
+  'r', 'radius', 'refX', 'refY', 'renderingIntent', 'repeatCount', 'repeatDur', 'requiredExtensions',
+  'requiredFeatures', 'restart', 'result', 'rotate', 'rx', 'ry', 'scale', 'seed', 'shapeRendering', 'slope',
+  'spacing', 'specularConstant', 'specularExponent', 'speed', 'spreadMethod', 'startOffset',
+  'stdDeviation', 'stemh', 'stemv', 'stitchTiles', 'stopColor', 'stopOpacity',
+  'strikethroughPosition', 'strikethroughThickness', 'string', 'stroke', 'strokeDasharray',
+  'strokeDashoffset', 'strokeLinecap', 'strokeLinejoin', 'strokeMiterlimit', 'strokeOpacity',
+  'strokeWidth', 'surfaceScale', 'systemLanguage', 'tableValues', 'targetX', 'targetY', 'textAnchor',
+  'textDecoration', 'textLength', 'textRendering', 'to', 'transform', 'u1', 'u2', 'underlinePosition',
+  'underlineThickness', 'unicode', 'unicodeBidi', 'unicodeRange', 'unitsPerEm', 'vAlphabetic',
+  'vHanging', 'vIdeographic', 'vMathematical', 'values', 'vectorEffect', 'version', 'vertAdvY',
+  'vertOriginX', 'vertOriginY', 'viewBox', 'viewTarget', 'visibility', 'widths', 'wordSpacing',
+  'writingMode', 'x', 'x1', 'x2', 'xChannelSelector', 'xHeight', 'xlinkActuate', 'xlinkArcrole',
+  'xlinkHref', 'xlinkRole', 'xlinkShow', 'xlinkTitle', 'xlinkType', 'xmlns', 'xmlnsXlink', 'xmlBase',
+  'xmlLang', 'xmlSpace', 'y', 'y1', 'y2', 'yChannelSelector', 'z', 'zoomAndPan'];
 
-// Create a map of HTML attribute to React prop
-// e.g. {"datetime": "dateTime"}
-const attributeMap = supportedAttributes.reduce((map, reactAttribute) => {
-    const htmlAttribute = reactAttribute.toLowerCase();
-    map[htmlAttribute] = reactAttribute;
 
-    return map;
-},
-    // Start the map with two attributes that have special names in React, and
-    // two attributes that already have camelCasing in HTML.
-    {
-        'class': 'className',
-        'for': 'htmlFor',
-        'httpEquiv': 'httpEquiv',
-        'acceptCharset': 'acceptCharset'
+// https://github.com/royriojas/flat-cache#readme
+
+var pageCache = flatCache.load('pages', path.resolve('cache'));
+
+const CACHE_TIMEOUT = 1000 * 24 * 60 * 60
+
+const cacheSave = function(key, page) {
+  const timeStamp = Date.now() + CACHE_TIMEOUT;
+  const value = {timeStamp, page}
+  pageCache.setKey(key, value)
+  pageCache.save(true)
+}
+
+const cacheFetch = function(key) {
+  const value = pageCache.getKey(key)
+  if (value) {
+    if (value.timeStamp > Date.now()) {
+      return value.page
     }
-);
+    pageCache.removeKey(key);
+    pageCache.save()
+  }
+  return undefined;
+}
+
+
+const fetchPage = async function(pageURL) {
+  let page = cacheFetch(pageURL)
+  if (! page) {
+    console.log('Fetching %s', pageURL)
+    try {
+      page = await request(pageURL)
+      cacheSave(pageURL, page)
+    } catch (error) {
+      console.log("Error loading %s", pageURL)
+      page = "?????"
+    }
+  }
+  return page
+}
+
+const getElementDetails = async function(href) {
+  const pageURL = 'https://developer.mozilla.org' + href;
+  try {
+    const html = await fetchPage(pageURL)
+    const $ = cheerio.load(html);
+    const desc = $('p', 'article').text()
+    return desc.split('.')[0]
+  } catch (error) {
+    console.log("Error loading %s", pageURL)
+    return "?????"
+  }
+}
 
 /**
  * From the MDN attributes reference, extract a map of attributes with
  * descriptions and supported elements.
  */
-function extractAttributes($) {
-    const $table = $('#Attribute_list,#attribute_list').parent().find('table');
-    if($table.length !== 1) {
-        throw new Error('page structure changed at ' + htmlURL);
-    }
-    const attributes = {};
+const extractAttributes = async function($) {
 
-    $table.find('tbody tr').each((i, row) => {
-        const $children = cheerio(row).find('td');
-        const attribute = $children.eq(0).text();
+  const attributeList = [];
 
-        const elements = $children.eq(1).text()
-            .replace(/[<>\s]/g, '')
-            .split(',');
+  "abcdefghijklmnopqrstuvwzyz".split('').forEach(letter => {
+    const attributes = $(`h3[id=${letter}]`).next().find('a')
+    attributes.each((i, attr) => {
+      // console.log(cheerio(attr).text())
 
-        const description = $children.eq(2).text()
-            .replace(/\n/g, '')
-            // Fix irregular whitespace characters
-            .replace(/\s+/g, ' ')
-            .trim();
+      const svgAttribute = str(cheerio(attr).text())
+        .trim()
+        // Convert e.g. `accept-charset` to `acceptCharset`
+        .camelize()
+        .toString();
 
-        const htmlAttribute = str(attribute)
-            .trim()
-            // Convert e.g. `accept-charset` to `acceptCharset`
-            .camelize()
-            .toString();
+      // Ensure attribute is supported by React
+      if (!supportedAttributes.includes(svgAttribute)) {
+        return;
+      }
 
-        // Skip `data-*` attributes
-        if (htmlAttribute.indexOf('data-') === 0) {
-            return;
-        }
-
-        // Ensure attribute is supported by React
-        if (!attributeMap[htmlAttribute]) {
-            return;
-        }
-
-        // maxlength -> maxLength; class -> className
-        const reactAttribute = attributeMap[htmlAttribute];
-
-        attributes[reactAttribute] = {
-            elements,
-            description
-        };
+      const pageUrl = attr.attribs.href
+      attributeList.push({svgAttribute, pageUrl})
 
     });
+  });
 
-    return attributes;
+  for (const i in attributeList) {
+    const attr = attributeList[i]
+    const description = await getElementDetails(attr.pageUrl)
+    console.log("[%s]\n%s\n", attr.svgAttribute, description)
+  }
+
+  return attributeList;
 }
 
 /**
@@ -123,24 +162,34 @@ function extractElements(attributes) {
 
 // A local copy of the MDN attributes web page has been saved for reference:
 // fs.readFile('./data/attributes.html', 'utf-8', (error, html) => {
-request(htmlURL, (error, response, html) => {
-    if (error) {
-        throw error;
-    }
+
+const extractAllAttributes = async function() {
+
+  try {
+    const html = await fetchPage(svgURL)
     const html2 = html.split(/\n\r|\r\n|\r|\n/).map(l => l.trimRight()).join('\n');
+
     // write back to the saved copy of MDN attributes so we can see the diff
+
     fs.writeFileSync(htmlPath, html2);
 
     const $ = cheerio.load(html);
-    const attributes = extractAttributes($);
+    const attributes = await extractAttributes($);
     const elements = extractElements(attributes);
+
     const out = {
-        attributes,
-        elements
+      attributes,
+      elements
     };
 
     // Print out JSON with 4-space indentation formatting.
     // http://stackoverflow.com/a/11276104
     const tabWidth = 4;
     fs.writeFileSync(dataPath, JSON.stringify(out, null, tabWidth));
-});
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+extractAllAttributes()
